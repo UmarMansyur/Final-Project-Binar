2edd2a7fa64be421a5d0c7fd1697e13610e2a815
johnsihombing42 <johnsihombing42@gmail.com>
2022-11-22 20:38:36 +0700

    init develop project

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-node_modules.env;
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const router = require("./routes");
+const app = express();
+const { HTTP_PORT = 3000 } = process.env;
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(router);
+
+// 404 handler
+app.use((req, res, next) => {
+  return res.status(404).json({
+    status: false,
+    message: "Are you lost?",
+  });
+});
+
+// 500 handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  return res.status(500).json({
+    status: false,
+    message: err.message,
+  });
+});
+
+app.listen(HTTP_PORT, () => console.log("listening on port", HTTP_PORT));
