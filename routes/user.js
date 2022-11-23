1a47840d8a9ce93f3f4237fe04593c9df15cc899
fadil199 <achmadfadilla112@gmail.com>
2022-11-23 19:53:49 +0700

    feat: register

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const cont = require("../controllers");
 const middle = require("../middlewares/authorize");
+const roles = require('../utils/roles')
 
 // router.get('/register', cont.user.register);
 router.post('/register', cont.user.register);
@@ -9,6 +10,6 @@ router.post('/register', cont.user.register);
 // router.get('/login', cont.auth.signIn);
 router.post('/login', cont.user.login);
 
-router.get('/auth', cont.user.auth);
+router.get('/auth', middle(roles.user), cont.user.auth);
 
 module.exports = router;
