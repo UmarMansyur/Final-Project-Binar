fe1ef87c19c58c415b63914ba68240e85ab379d4
fadil199 <achmadfadilla112@gmail.com>
2022-11-28 21:49:17 +0700

    feat: add update profile

---
+++ models/flight.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Flight extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Flight.init({
+    airlineName: DataTypes.STRING,
+    departureCity: DataTypes.STRING,
+    arrivalCity: DataTypes.STRING,
+    departureCity: DataTypes.STRING,
+    departureTime: DataTypes.TIME,
+    arrivalTime: DataTypes.STRING,
+    totalSeat: DataTypes.INTEGER,
+    class: DataTypes.STRING,
+    gate: DataTypes.STRING,
+    boardingTime: DataTypes.TIME,
+    price: DataTypes.INTEGER,
+    stock: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Flight',
+  });
+  return Flight;
+};
---
+++ models/notification.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Notification extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Notification.init({
+    user_id: DataTypes.INTEGER,
+    title: DataTypes.STRING,
+    detail_message: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Notification',
+  });
+  return Notification;
+};
