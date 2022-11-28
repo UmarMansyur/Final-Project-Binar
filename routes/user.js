f5aa0225d976387bcb8cc298795ffb1a639ac76c
Muhammad Umar Mansyur <umar@MacBook-Air-Muhammad.local>
2022-11-28 20:13:33 +0700

    added loginWithGoogle

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,8 @@ router.post('/register', cont.user.register);
 
 // router.get('/login', cont.auth.signIn);
 router.post('/login', cont.user.login);
-
+router.get('/login/google', cont.user.loginGoogle);
+router.get('/login/facebook', cont.user.loginFacebook);
 router.get('/auth', middle(roles.user), cont.user.auth);
 
 router.post('/subscribe', cont.webpush.webPush);
