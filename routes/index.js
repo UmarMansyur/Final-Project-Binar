412d2b20c136d8ff3d2e12e38cb32d8b652162b9
johnsihombing42 <johnsihombing42@gmail.com>
2022-12-02 21:29:37 +0700

    add index route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const user = require("./user");
 const auth = require("./auth");
 
+router.get("/", (req, res) => {
+  return res.status(200).json({
+    status: true,
+    message: "Welcome to Terbang Tinggi API",
+  });
+});
+
 router.use("/auth", auth);
 router.use("/user", user);
 
