c65a867a023968607e0ff04657e268048b5d67d6
johnsihombing42 <johnsihombing42@gmail.com>
2022-12-02 21:26:25 +0700

    redirect forgot page to frontend

--- controllers/auth.js
+++ controllers/auth.js
@@ -59,8 +59,8 @@ module.exports = {
       });
 
       await DetailUser.create({
-        user_id: user.id
-      })
+        user_id: user.id,
+      });
 
       const apiHost = API_HOST;
       const payload1 = { id: user.id };
@@ -261,7 +261,7 @@ module.exports = {
       } else {
         const payload = { user_id: user.id };
         const token = jwt.sign(payload, JWT_SECRET_KEY);
-        const link = `${apiHost}/auth/reset-password?token=${token}`;
+        const link = `https://terbangtinggi-staging.km3ggwp.com/reset-password?token=${token}`;
         htmlEmail = await email1.getHtml("email/reset-password.ejs", {
           name: user.name,
           link: link,
