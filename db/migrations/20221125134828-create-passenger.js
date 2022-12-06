c8574d95408778ae65b5ca6b48c3d64e08026e65
fadil199 <achmadfadilla112@gmail.com>
2022-12-06 20:48:42 +0700

    fix code

--- db/migrations/20221125134828-create-passenger.js
+++ db/migrations/20221125134828-create-passenger.js
@@ -8,6 +8,9 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      passenger_id: {
+        type: Sequelize.INTEGER
+      },
       email: {
         type: Sequelize.STRING
       },
