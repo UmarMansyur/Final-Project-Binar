e7c82a9a867e91206b8be4b272a5402301296581
fadil199 <achmadfadilla112@gmail.com>
2022-12-06 18:14:35 +0700

    feat: add detail flight endpoint

--- routes/flight.js
+++ routes/flight.js
@@ -11,4 +11,6 @@ router.get("/data", middle([roles.admin]), cont.flight.read);
 router.put("/data/:flightId", middle([roles.admin]), cont.flight.update);
 
 router.delete("/data/:flightId", middle([roles.admin]), cont.flight.delete);
+
+router.get("/myFlight/:id", middle([roles.user, roles.admin]), cont.flight.detailFlight);
 module.exports = router;
