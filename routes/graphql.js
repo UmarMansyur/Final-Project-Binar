cf0ee6f99e2d86bff2719954316a706c68bdab07
Muhammad Umar Mansyur <umar@192.168.1.5>
2022-12-08 10:52:38 +0700

    fixed id

--- routes/graphql.js
+++ routes/graphql.js
@@ -4,7 +4,7 @@ const schema = require("./schema");
 
 const root = {
     user: async ({id, offset = 0, limit = 5, order = 'createdAt'}) => {
-        if (id) await User.findOne({ where: { id } })
+        if (id) return await User.findOne({ where: { id } })
         return await User.findAll({ offset: offset, limit : limit, order: [order] });
     },
     
