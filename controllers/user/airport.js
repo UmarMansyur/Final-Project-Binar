359bc5bfdcaf429bc6e744d883c9dcb8e25bd6aa
johnsihombing42 <johnsihombing42@gmail.com>
2022-12-13 16:51:32 +0700

    filter airport indonesia

--- controllers/user/airport.js
+++ controllers/user/airport.js
@@ -37,7 +37,6 @@ module.exports = {
 
   getIndoAirport: async (req, res, next) => {
     try {
-      const { search } = req.params;
       const url =
         "https://gist.githubusercontent.com/aroyan/89b170c9f76df8f76c31d3fe1b1d33df/raw/361448fca988b0c1d9ff6b3c5d0f51dbeeda388a/allAirportInIndonesia.json";
       const options = {
@@ -49,12 +48,25 @@ module.exports = {
       };
       const result = await fetch(url, options);
       const json = await result.json();
-      const data = json;
+      const airports = json;
 
+      const indonesiaAirport = [];
+      for (const airport of airports) {
+        const schedule = {
+          airportCode: airport.airportCode,
+          airportName: airport.airportName,
+          airportLocation: airport.airportLocation,
+          cityName: airport.cityName,
+          countryName: airport.countryName,
+          alias: airport.alias,
+        };
+
+        indonesiaAirport.push(schedule);
+      }
       return res.status(200).json({
         status: true,
         message: "Success Get Data",
-        data: data,
+        data: indonesiaAirport,
       });
     } catch (error) {
       next(err);
