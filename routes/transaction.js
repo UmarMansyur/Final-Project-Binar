86fd7cfae4b1c1e8679b3a3149a6db1a56cf4903
johnsihombing42 <johnsihombing42@gmail.com>
2022-12-15 11:32:18 +0700

    req.user

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,13 +1,13 @@
-const router = require('express').Router();
-const cont = require('../controllers/user');
+const router = require("express").Router();
+const cont = require("../controllers/user");
 const middle = require("../middlewares/authorize");
-const roles = require('../utils/roles');
+const roles = require("../utils/roles");
 
 // router.post('/', middle(roles.user) ,cont.transaction.createTransaction);
-router.post('/' ,cont.transaction.createTransaction);
-router.get('/', cont.transaction.show);
-router.get('/:id', cont.transaction.getTransactionById);
-router.put('/:id', cont.transaction.update);
-router.delete('/:id', cont.transaction.delete);
+router.post("/", middle(roles.user), cont.transaction.createTransaction);
+router.get("/", cont.transaction.show);
+router.get("/:id", cont.transaction.getTransactionById);
+router.put("/:id", cont.transaction.update);
+router.delete("/:id", cont.transaction.delete);
 
 module.exports = router;
