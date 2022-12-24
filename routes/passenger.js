430550bd84cc72eff2eb6ed5959043844dfdfd5d
johnsihombing42 <johnsihombing42@gmail.com>
2022-12-24 10:44:52 +0700

    update method upload document

--- routes/passenger.js
+++ routes/passenger.js
@@ -3,14 +3,22 @@ const router = express.Router();
 const cont = require("../controllers/user");
 const multer = require("multer");
 const upload = multer();
-const imagekit = require('../utils/imagekit');
+const imagekit = require("../utils/imagekit");
 const middle = require("../middlewares/authorize");
 const roles = require("../utils/roles");
-const storage = require('../utils/storage')
+const storage = require("../utils/storage");
 
 router.get("/myTicket", middle(roles.user), cont.passenger.show);
-router.get("/document/:payment_code", middle(roles.user), cont.passenger.getAllDocument)
-router.patch('/upload-document/:id', upload.single('image'), middle(roles.user), cont.passenger.uploadDocument);
-
+router.get(
+  "/document/:payment_code",
+  middle(roles.user),
+  cont.passenger.getAllDocument
+);
+router.post(
+  "/upload-document/:id",
+  upload.single("image"),
+  middle(roles.user),
+  cont.passenger.uploadDocument
+);
 
 module.exports = router;
