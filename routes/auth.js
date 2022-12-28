626bf0fe56f4abd72bd0fa9b5fadad79b2690b38
johnsihombing42 <johnsihombing42@gmail.com>
2022-12-28 23:25:37 +0700

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ router.post("/login", cont.auth.login);
 router.post("/google", cont.auth.loginGoogle);
 router.get("/login/google", cont.auth.loginGoogleGetData);
 router.get("/login/facebook", cont.auth.loginFacebook);
-router.patch("/reset-password", cont.auth.resetPassword);
+router.patch("/change-password", cont.auth.resetPassword);
 router.post("/forgot-password", cont.auth.forgotPassword);
 router.get("/me", middle([roles.user, roles.admin]), cont.auth.auth);
 router.post("/subscribe", cont.webpush.webPush);
